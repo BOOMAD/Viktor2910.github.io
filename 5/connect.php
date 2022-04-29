55fbfa5f1d1bc2ceb4870540de600355706adb74
ViktorKoltsov <90630784+BOOMAD@users.noreply.github.com>
2022-04-29 20:50:13 +0300

    Update connect.php

--- 5/connect.php
+++ 5/connect.php
@@ -1,4 +1,4 @@
 <?php
-$user = 'u41026';
-$pass = '4433573';
+$user = 'u41121';
+$pass = '467446357';
 $db = new PDO('mysql:host=localhost;dbname=u41026', $user, $pass, array(PDO::ATTR_PERSISTENT => true)); ?>
