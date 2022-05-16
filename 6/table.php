a6db18aa83bb942eb8514fde4f338b5ce6c43470
ViktorKoltsov <90630784+BOOMAD@users.noreply.github.com>
2022-05-16 23:30:12 +0300

    Update table.php

--- 6/table.php
+++ 6/table.php
@@ -22,6 +22,30 @@ background: #f5f5f5;
 th:first-child, td:first-child {
 text-align: left;
 }
+  .animation{
+text-aligh: centre;
+margin-top 15px;
+font-family: Arial;
+font-size:80px;
+text-transform: uppercase;
+color: rgba(255,255,255,0.2);
+background: url(https://yandex.ru/images/search?pos=1&img_url=https%3A%2F%2Fget.pxhere.com%2Fphoto%2Fwarm-orange-red-flame-fire-romantic-cozy-fireplace-glow-darkness-campfire-heat-energy-burn-performance-art-blaze-firelight-fiery-embers-wood-fire-flame-log-fire-heiss-wood-for-the-fireplace-open-fire-computer-wallpaper-1376000.jpg&text=пламя&lr=35&rpt=simage&source=serp);
+repeat-x;
+-webkit-background-clip: text;
+background-size: contain;
+animation: fire 13s linear infinite;
+}
+@keyframes fire{
+0%{
+background-position: left 0 top 0;
+}
+50%{
+background-position: left 150px top -25px;
+}
+100%{
+background-position: left 300px top 0;
+}
+}
   .messega{
     color: #ff4411; font-family: ‘Lucida Sans’, Arial, sans-serif; font-size: 16px; line-height: 26px; text-indent: 30px; margin: 0;
   }
@@ -33,6 +57,7 @@ text-align: left;
   }
 </style>
 <body>
+  <h1 class="animation">Вы успешно авторизовались и видите защищенные паролем данные</h1>
   <div class="table1">
     <table>
       <tr>
